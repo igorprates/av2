5a042085b213b4e06ac11750bb106dcb73b8e43b
Michelly Ponce <Michelly Ponce@192.168.25.5>
2017-06-10 01:10:26 -0300

    inserindo algumas atividades para complemento da nota av1

--- RevisaoProva/src/TestandoOrientacaoaobjeto/Carro.java
+++ RevisaoProva/src/TestandoOrientacaoaobjeto/Carro.java
@@ -14,6 +14,7 @@ public class Carro {
     String modelo;
     double velocidadeAtual;
     double velocidadeMaxima;
+    Motor motor;
     
     //liga o carro
     void liga () {
---
+++ RevisaoProva/src/TestandoOrientacaoaobjeto/Motor.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestandoOrientacaoaobjeto;
+
+/**
+ *
+ * @author Michelly Ponce
+ */
+public class Motor {
+ int potencia;
+ String tipo;
+}
