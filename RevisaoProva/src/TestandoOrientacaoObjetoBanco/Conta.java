04b441cfa91110edc1cd0a505b9e10428984db6b
Michelly Ponce <Michelly Ponce@192.168.25.5>
2017-06-10 01:28:55 -0300

    inserindo algumas atividades para complemento da nota av1

---
+++ RevisaoProva/src/TestandoOrientacaoObjetoBanco/Conta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestandoOrientacaoObjetoBanco;
+
+/**
+ *
+ * @author Michelly Ponce
+ */
+public class Conta {
+    int numero;
+    String dono;
+    double saldo;
+    double limite;
+}
+
+
+
---
+++ RevisaoProva/src/TestandoOrientacaoObjetoBanco/Programa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestandoOrientacaoObjetoBanco;
+
+/**
+ *
+ * @author Michelly Ponce
+ */
+public class Programa {
+
+    public static void main(String[] args) {
+        Conta minhaConta;
+        minhaConta = new Conta();
+        
+        minhaConta.dono = "Igor";
+        minhaConta.saldo = 2000.0;
+        
+        System.out.println("Saldo atual: " + minhaConta.saldo);
+        
+    }
+}
+
